cfe9fba38ccfb4fd69bbed186b5581a3b026639c
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-25 18:33:56 -0700

    Find the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ mystring = "Last digit of {} is {}".format(number, last_num)
 if number > 5:
     print(f"{mystring} and is greater than 5")
 elif number == 0:
-    print (f"{mystring} and is 0")
+    print(f"{mystring} and is 0")
 elif number < 6:
     print(f"{mystring} and is less than 6 and not 0")
