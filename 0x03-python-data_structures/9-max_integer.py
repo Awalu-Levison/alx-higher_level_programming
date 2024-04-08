041bf7a31b9c2f05dac64990540a654fa6dc17ba
Awalu-Levison <levisonawalu251@gmail.com>
2024-04-07 22:05:02 -0700

    Get the maximum number from the list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        large_number = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > large_number:
+                large_number = my_list[i]
+        return large_number
