32171b74bf0e038b645379b00ebfdabb65aaf07d
Awalu-Levison <levisonawalu251@gmail.com>
2024-04-18 14:33:24 -0700

    Covert roman numerals to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    romans = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     temp_number = 0
     if type(roman_string) is not str or roman_string is None:
         return (0)
     for j, char in enumerate(roman_string):
-        temp_value = roman_dict[char]
+        temp_value = romans[char]
         try:
             # Get the minus values of roman numerals
-            if temp_number < roman_dict[roman_string[j + 1]]:
+            if temp_number < romans[roman_string[j + 1]]:
                 temp_number = temp_number * -1
         except IndexError:
             pass
-        temp_number +=  temp_value
+        temp_number += temp_value
     return temp_number
