254cd7309c72bad9cb74d11d4c4f66c4e63502fb
Awalu-Levison <levisonawalu251@gmail.com>
2024-05-26 01:43:13 -0700

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
+
     def area(self):
         """defining the public method
         method:
