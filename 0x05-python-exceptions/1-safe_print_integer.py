6ce170aae03cd89d0babb6060d7e15209d5f43a2
Awalu-Levison <levisonawalu251@gmail.com>
2024-06-02 14:17:39 -0700

    Safe print_print integers only

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """print integers only
+    if not, control the error with an exception
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
