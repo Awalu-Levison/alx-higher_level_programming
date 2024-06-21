8c2c08b5de62651c8872452c1930b5d7ee03f42a
Awalu-Levison <levisonawalu251@gmail.com>
2024-06-20 18:30:05 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myArgs = process.argv;
+if (myArgs == argv[0]) {
+	console.log('No argument');
+}
+else if (myArgs == argv[1]) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
