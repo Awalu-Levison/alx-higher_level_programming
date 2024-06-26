53eb59e277b37227ec94a11be751fe9ee6e3b348
Awalu-Levison <levisonawalu251@gmail.com>
2024-06-26 16:30:39 -0700

    Organise files

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const user_data = process.argv[2];
-const num = Number(user_data);
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${Math.floor(num)}`);
-}
