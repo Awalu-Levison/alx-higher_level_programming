5a655afca7d31efb7386568be2971e5ac6a01bba
Awalu-Levison <levisonawalu251@gmail.com>
2024-06-30 13:06:02 -0700

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const x = Math.floor(Number(process.argv[2]));
+
+if (isNaN(x)) { /*Check if argv[2] is provided*/
+  console.log('Missing size');
+  } else {
+     for (let row1 = 0; row1 < x; row1++) {
+       let row2 = '';
+      for (let i = 0; i < x; i++)
+        row2 += 'X';
+        console.log(row2);
+    }
+}   
