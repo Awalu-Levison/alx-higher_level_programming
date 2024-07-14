69632ac3313b8163607a25292eb0d22d03e81942
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-14 09:36:55 -0700

    Find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # Find the peak of unsorted numbers in a list
+
+
 def find_peak(list_of_integers):
     """Find the peak of unsorted integers"""
     if list_of_integers == []:
@@ -13,15 +15,11 @@ def find_peak(list_of_integers):
     elif size == 2:
         return max(list_of_integers)
 
-    """Find middle number"""
     mid = int(size/2)
     peak = list_of_integers[mid]
     newlist = list_of_integers
-
-    """Find the peak"""
     if peak > newlist[mid - 1] and peak > newlist[mid + 1]:
         return peak
-
     elif peak < newlist[mid - 1]:
         return find_peak(newlist[:mid])
     else:
