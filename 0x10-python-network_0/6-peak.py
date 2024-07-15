b1e5f952e07a71a8c95c7dd8c6cccb48d791e30d
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-15 14:20:13 -0700

    Find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,8 +6,6 @@ def find_peak(list_of_integers):
     """Goes through the list of unsorted integers to find the peak"""
     if not list_of_integers:
         return None
-    if list_of_integers:
+    else:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
-    else:
-        return None
