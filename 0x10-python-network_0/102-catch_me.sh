b84b3e203a6907349a53f0fa7a7adf85c06cc336
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-16 19:40:39 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that makes a request to 0.0.0.0:5000/catch_me, display text of "You got me!"
-curl -s -L -X PUT -H "HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
