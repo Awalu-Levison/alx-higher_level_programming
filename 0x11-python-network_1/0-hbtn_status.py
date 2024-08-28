9ab51d74ed03b1cbb673b5e2fe88e8b17a2be073
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-28 00:11:43 -0700

    Whats my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ Fetch data from https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
+
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
     html = res.read()
 print('Body response:')
