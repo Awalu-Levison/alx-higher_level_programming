6ca8a8d8b3b8e7621400de1fb1ffd7fae3b3d66a
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-29 03:19:48 -0700

    Whats my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,10 +8,11 @@ The body of the response must be display like
 the following example (tabulation before -)
 """
 
+
 import requests
 
 if __name__ == "__main__":
     user_url = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print("\t- type: {}".format(type(user_url.text)))
-    print("\t- content: {}".format(type(user_url.text)))
+    print("\t- content: {}".format(user_url.text))
