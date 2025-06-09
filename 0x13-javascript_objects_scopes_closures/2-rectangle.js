265c402c1ea31264762f6b506a814cbceb47b318
Awalu-Levison <levisonawalu251@gmail.com>
2025-06-09 16:59:25 +0200

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,16 @@
 
 class Rectangle {
   constructor(w, h) {
+    h
     if (
         typeof w !== 'number' || typeof h !== 'number' ||
         !Number.isInteger(w) || !Number.isInteger(h) ||
         w <= 0 || h <= 0
-    ) {
+    )
+    {
         return {};
     }
-    this.width = w;
-    this.height = h;
-}
+        this.width = w;
+        this.height = h;
+  }
 }
