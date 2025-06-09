fa9485d75e370af0e76bcaa2592f1abc44df960c
Awalu-Levison <levisonawalu251@gmail.com>
2025-06-09 22:57:45 +0200

    Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const newSquare = require('./5-square');
+
+class Square extends newSquare {
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    }
+
+    else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
