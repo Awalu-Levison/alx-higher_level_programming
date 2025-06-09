f064605c01a139faf32da2c0ffbfea5b0a50761b
Awalu-Levison <levisonawalu251@gmail.com>
2025-06-09 21:59:56 +0200

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,29 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w >= 0 || h >= 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
      const newHeight = this.width;
      this.width = this.height;
      this.height = newHeight;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
 }
+
 module.exports = Rectangle;
